4ab71fa80b5fda6f41b76568704ac7df9c657417
Nidhi Gupta <nidhigupta@Nidhis-MacBook-Pro.local>
2025-03-05 19:37:59 -0500

    fix world service names

--- WorldService/src/main/java/helloservice/WorldServiceApplication.java
+++ WorldService/src/main/java/helloservice/WorldServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloServiceApplication {
+public class WorldServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloServiceApplication.class, args);
+        SpringApplication.run(WorldServiceApplication.class, args);
     }
 
 }
--- WorldService/src/main/java/helloservice/controllers/WorldController.java
+++ WorldService/src/main/java/helloservice/controllers/WorldController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class WorldController {
 
     @Autowired
     private MeterRegistry meterRegistry;  // Inject the MeterRegistry
 
     // Custom metric to count the number of requests to /hello
     @GetMapping("/world")
-    public String getHello() {
+    public String getWorld() {
         // Increment the custom counter metric
         Counter helloCounter = meterRegistry.counter("requests.world");
         helloCounter.increment();  // Increment every time this endpoint is hit
